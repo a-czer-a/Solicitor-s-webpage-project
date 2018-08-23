9e387a4828e8862c34ae0cb59ac09273fcbaa2a8
Agata Czerwińska <agata@rubrica.pl>
2018-08-23 21:40:16 +0200

    Add initial files

---
+++ js/bin/scripts.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, {});
+
+    var elems = document.querySelectorAll('.scrollspy');
+    var instances = M.ScrollSpy.init(elems, {
+      scrollOffset: 50
+    });
+  });
